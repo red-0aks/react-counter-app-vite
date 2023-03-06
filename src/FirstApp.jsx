e463a15b4aa79fb4e817b7184779ef74a97ba74d
fsepulve <fsepulvb@emeal.nttdata.com>
2023-03-06 20:51:54 -0300

    Commit inicial del repositorio

---
+++ src/FirstApp.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+export const FirstApp = ({title, subtitle}) => {
+
+   return (
+      <>
+         <h1>{ title }</h1>
+         <p>{ subtitle }</p>
+      </>
+   )
+}
+
+FirstApp.propTypes = {
+   title: PropTypes.string.isRequired,
+   subtitle: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+   title: 'No hay titulo',
+   subtitle: 1
+}
